8fab999ee581790f026524817a6d9edc18e8dc45
Jon Kolman <jonathankolman@gmail.com>
2017-03-11 00:12:36 +0100

    updated tests

--- test/igbot.js
+++ test/igbot.js
@@ -16,16 +16,11 @@ describe('ig bot', () => {
     getInstagramProfile('jkol36')
     .then(instagramProfile => {
       const {
-        followedBy, 
-        follows,
-        id,
-        instagramUsername,
-        initialPicIds
+        user:{followed_by, full_name, username}
       } = instagramProfile
-      expect(id).to.not.be.undefined
-      expect(follows).to.not.be.undefined
-      expect(instagramUsername).to.not.be.undefined
-      expect(initialPicIds).to.be.an('array')
+      expect(followed_by).to.not.be.undefined
+      expect(full_name).to.not.be.undefined
+      expect(username).to.not.be.undefined
       done()
     })
   })
@@ -40,23 +35,7 @@ describe('ig bot', () => {
       done()
     })
   })
-  it('should find user from pic', done => {
-    let picCode = 'BRYW5mWhC7D'
-    findUserFromInstagramPic(picCode, 'startups', headers)
-    .then(user => {
-      expect(user).to.be.an('object')
-      expect(user).to.have.property('id')
-      expect(user).to.have.property('username')
-      expect(user).to.have.property('has_blocked_viewer')
-      expect(user).to.have.property('blocked_by_viewer')
-      expect(user).to.have.property('requested_by_viewer')
-      expect(user).to.have.property('is_private')
-      expect(user).to.have.property('full_name')
-      expect(user).to.have.property('is_unpublished')
-      done()
-    })
-    .catch(done)
-  })
+
   it('should get next pics for hashtag startups', done => {
     getNextPicsForHashtag('startups', 'J0HWMAiSQAAAF0HWL9-egAAAFiYA',12)
     .then(picsAndPageInfo => {
@@ -80,10 +59,9 @@ describe('ig bot', () => {
   })
   it('should get initial instagram following for a user', done => {
     getInstagramFollowing('54537579', 12)
-    .then(followersAndPageInfo => {
-      const {followerArray, pageInfo} = followersAndPageInfo
-      console.log(followerArray[0])
-      expect(followerArray).to.be.an('array')
+    .then(followingAndPageInfo => {
+      const {followingArray, pageInfo} = followingAndPageInfo
+      expect(followingArray).to.be.an('array')
       expect(pageInfo).to.be.an('object')
       expect(pageInfo).to.have.property('end_cursor')
       done()
